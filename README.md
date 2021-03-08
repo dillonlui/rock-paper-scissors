865dbfb5fb4a627b8e7b62d5aa7322d05cc014db
Dillon Lui <dillonglui@gmail.com>
2021-03-08 17:08:47 -0600

    Added UI to game

--- README.md
+++ README.md
@@ -3,3 +3,5 @@ Rock, Paper, Scissors Project
 
 What I learned:
 - How to return a random element from an array.
+- Incorporating loops
+- Calling functions within functions
--- app.js
+++ app.js
@@ -1,55 +1,108 @@
+const btnRock = document.querySelector('#rock');
+const btnPaper = document.querySelector('#paper');
+const btnScissors = document.querySelector('#scissors');
+const totalScore = document.querySelector('#totalScore');
+const results = document.querySelector('#results');
+let playerScore = document.querySelector('#playerScore');
+let computerScore = document.querySelector('#computerScore');
+
+// Rock-Paper-Scissors Game Logic
+
+function computerPoint() {
+    computerScore.innerText = parseInt(computerScore.innerText) + 1
+}
+function playerPoint() {
+    playerScore.innerText = parseInt(playerScore.innerText) + 1
+}
+function gameResult(gameOutput) {
+    let gameResult = document.createElement('li')
+    gameResult.innerText = gameOutput;
+    results.appendChild(gameResult)
+}
+
 function computerPlay() {
     const myArray = ['Rock', 'Paper', 'Scissors'];
     return myArray[Math.floor(Math.random() * myArray.length)];
 }
 
-function playRound(playerSelection, computerSelection) {
-    const playerInput = prompt("Rock, Paper, or Scissors?").toLowerCase();
+function playRound(playerInput) {
     computerInput = computerPlay().toLowerCase();
 
     if (playerInput == computerInput) {
-        console.log(`Computer played: ${computerInput}, tie Game!`)
-        return 'tie'
+        let gameOutput = (`Computer played: ${computerInput}, tie game!`)
+        gameResult(gameOutput)
     } else if (playerInput == 'rock' && computerInput == 'paper') {
-        console.log(`Computer played: ${computerInput}, you lose! Paper beats rock!`)
-        return 'lose'
+        let gameOutput = (`Computer played: ${computerInput}, you lose! Paper beats rock!`)
+        computerPoint();
+        gameResult(gameOutput)
     } else if (playerInput == 'paper' && computerInput == 'scissors') {
-        console.log(`Computer played: ${computerInput}, you lose! Scissors beats paper!`)
-        return 'lose'
+        let gameOutput = (`Computer played: ${computerInput}, you lose! Scissors beats paper!`)
+        computerPoint();
+        gameResult(gameOutput)
     } else if (playerInput == 'scissors' && computerInput == 'rock') {
-        console.log(`Computer played: ${computerInput}, you lose! Rock beats scissors!`)
-        return 'lose'
+        let gameOutput = (`Computer played: ${computerInput}, you lose! Rock beats scissors!`)
+        computerPoint();
+        gameResult(gameOutput)
     } else if (playerInput == 'rock' && computerInput == 'scissors') {
-        console.log(`Computer played: ${computerInput}, you win! Rock beats scissors!`)
-        return 'win'
+        let gameOutput = (`Computer played: ${computerInput}, you win! Rock beats scissors!`)
+        playerPoint();
+        gameResult(gameOutput)
     } else if (playerInput == 'paper' && computerInput == 'rock') {
-        console.log(`Computer played: ${computerInput}, you win! Paper beats rock!`)
-        return 'win'
+        let gameOutput = (`Computer played: ${computerInput}, you win! Paper beats rock!`)
+        playerPoint();
+        gameResult(gameOutput)
     } else if (playerInput == 'scissors' && computerInput == 'paper') {
-        console.log(`Computer played: ${computerInput}, you win! Scissors beats paper!`)
-        return 'win'
+        let gameOutput = (`Computer played: ${computerInput}, you win! Scissors beats paper!`)
+        playerPoint();
+        gameResult(gameOutput)
     } else {
         return 'Invalid input!'
     }
 }
 
-function game() {
-    let playerScore = 0;
-    let computerScore = 0;
-    for (let i = 1; i <= 5; i++) {
-        let result = playRound();
-        if (result == 'win') {
-            playerScore++
-        } else if (result == 'lose') {
-            computerScore++
-        }
-        console.log(`Player: ${playerScore}, Computer: ${computerScore}.`)
-    }
-    if (playerScore > computerScore) {
-        console.log(`Congrats! You beat the computer ${playerScore} to ${computerScore}!`)
-    } else if (playerScore < computerScore) {
-        console.log(`Sorry, you lost to the computer ${computerScore} to ${playerScore}. Better luck next time!`)
-    } else {
-        console.log(`Wow you tied the computer ${playerScore} to ${computerScore}!`)
+// User Interaction
+
+function checkScore() {
+    if (playerScore.innerText == 5) {
+        alert('Player wins, first to 5!');
+    } else if (computerScore.innerText == 5) {
+        alert('Computer wins, first to 5!');
     }
 }
+
+btnRock.addEventListener('click', function () {
+    playRound('rock')
+    checkScore();
+})
+btnPaper.addEventListener('click', function () {
+    playRound('paper')
+    checkScore();
+})
+btnScissors.addEventListener('click', function () {
+    playRound('scissors')
+    checkScore();
+})
+
+
+
+
+// function game() {
+//     let playerScore = 0;
+//     let computerScore = 0;
+//     for (let i = 1; i <= 5; i++) {
+//         let result = playRound();
+//         if (result == 'win') {
+//             playerScore++
+//         } else if (result == 'lose') {
+//             computerScore++
+//         }
+//         console.log(`Player: ${playerScore}, Computer: ${computerScore}.`)
+//     }
+//     if (playerScore > computerScore) {
+//         console.log(`Congrats! You beat the computer ${playerScore} to ${computerScore}!`)
+//     } else if (playerScore < computerScore) {
+//         console.log(`Sorry, you lost to the computer ${computerScore} to ${playerScore}. Better luck next time!`)
+//     } else {
+//         console.log(`Wow you tied the computer ${playerScore} to ${computerScore}!`)
+//     }
+// }
